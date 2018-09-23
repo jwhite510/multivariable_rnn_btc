813cb488ee98315f1e634bad02c028b195367a40
zom <jonathonwhite5@gmail.com>
2018-09-22 20:19:01 -0400

    added class for data storage

---
+++ datastorage.py
@@ -0,0 +1,76 @@
+import tables
+import os
+
+
+
+class RedditData():
+
+    def __init__(self, filename, parameters=None, overwrite=False, timecharlength=30, checklength=False):
+
+
+        self.filename = filename + '.hdf5'
+        self.parameters = parameters
+        self.timecharlength = timecharlength
+        self.checklength = checklength
+
+        # check if the file already exists
+        exists = os.path.exists('./{}'.format(self.filename))
+
+
+        # if not, create it
+        if not exists or overwrite:
+            print('creating hdf5 file {}'.format(self.filename))
+            # create file
+            hdf5_file = tables.open_file(self.filename, mode='w')
+
+            for parameter in self.parameters:
+
+                max_length = parameter['maxlength']
+                name = parameter['name']
+
+                hdf5_file.create_earray(hdf5_file.root, name, tables.StringAtom(itemsize=max_length), shape=(1, 0))
+                hdf5_file.create_earray(hdf5_file.root, name+'_time',
+                                        tables.StringAtom(itemsize=self.timecharlength), shape=(1, 0))
+
+            hdf5_file.close()
+
+        else:
+            pass
+
+
+    def append_data(self, parameter, data, times):
+
+        # append the file
+        hdf5_file = tables.open_file(self.filename, mode='a')
+
+        if self.checklength:
+            # check if the data is too long
+            max_length=None
+            for param in self.parameters:
+                if param['name'] == parameter:
+                    max_length = param['maxlength']
+
+            for data_entry in data:
+                if len(data_entry) > max_length:
+                    print('data exceeded allowable length')
+
+
+        # append the data
+        if len(data) == len(times):
+            getattr(hdf5_file.root, parameter).append([data])
+            getattr(hdf5_file.root, parameter+'_time').append([times])
+
+        hdf5_file.close()
+
+
+    def retrieve_data(self, parameter, indexes):
+
+
+        hdf5_file = tables.open_file(self.filename, mode='r')
+
+        values = getattr(hdf5_file.root, parameter)[0, indexes[0]:indexes[1]]
+        hdf5_file.close()
+
+        values_str = [element.decode('UTF-8') for element in values]
+
+        return(values_str)
--- reddit.py
+++ reddit.py
@@ -1,37 +1,53 @@
 import praw
 import reddit_credentials
+import tables
+from datastorage import RedditData
 
 
 
-reddit = praw.Reddit(client_id=reddit_credentials.CLIENT_ID,
-                     client_secret=reddit_credentials.CLIENT_SECRET,
-                     user_agent='agent')
 
 
-subreddit = reddit.subreddit('bitcoin')
-# hot = subreddit.hot(limit=5)
-#
-# for submission in hot:
-#     print(submission.title)
 
 
-for comment in subreddit.stream.comments():
-    try:
-
-        parent_id = str(comment.parent())
-        original = reddit.comment(parent_id)
-
-        print('Parent: ')
-        print('body: ', original.body)
-        print('submission: ', original.submission)
-        print(dir(original.submission))
-
-        print('Reply:')
-        print(comment.body)
 
 
-    except Exception as e:
-        print(e)
+
+if __name__ == "__main__":
+
+    # the data types that this file will contain
+    parameters = []
+    parameters.append({'name': 'comment', 'maxlength': 30})
+    parameters.append({'name': 'submission', 'maxlength': 100})
+
+    redditdata = RedditData(filename='redditdata', parameters=parameters, overwrite=True,
+                            checklength=True)
+
+
+    # collect the comments and times
+    comments = ['comment1body', 'comment2body', 'comment3body']
+    comment_times = ['comment time 1', 'comment time 2', 'comment time 3']
+
+    redditdata.append_data(parameter='comment', data=comments, times=comment_times)
+    redditdata.append_data(parameter='submission', data=comments, times=comment_times)
+
+
+    data = redditdata.retrieve_data(parameter='comment', indexes=(0, 2))
+    print(data)
+
+    data = redditdata.retrieve_data(parameter='comment_time', indexes=(0, 2))
+    print(data)
+
+    exit(0)
+
+    #
+    # # start the reddit instance
+    # reddit = praw.Reddit(client_id=reddit_credentials.CLIENT_ID,
+    #                      client_secret=reddit_credentials.CLIENT_SECRET,
+    #                      user_agent='agent')
+    #
+    #
+    # subreddit = reddit.subreddit('bitcoin')
+    #
 
 
 
