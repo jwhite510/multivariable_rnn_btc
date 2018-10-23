9de6509a6d06cb6204de8de5dbdb1d241146f64f
zom <jonathonwhite5@gmail.com>
2018-10-22 23:31:31 -0400

    added other coins

--- bittrex.py
+++ bittrex.py
@@ -37,17 +37,50 @@ if __name__ == "__main__":
     bittrexbtcprices = DataCollector(filename='bittrexbtcprices', parameters=parameters, overwrite=False,
                                   checklength=True)
 
+    parameters = []
+    parameters.append({'name': 'tetherprice', 'maxlength': 100})
+    bittrextetherprices = DataCollector(filename='bittrextetherprices', parameters=parameters, overwrite=False,
+                                     checklength=True)
+
+    parameters = []
+    parameters.append({'name': 'ethereumprice', 'maxlength': 100})
+    ethereumprices = DataCollector(filename='ethereumprices', parameters=parameters, overwrite=False,
+                                        checklength=True)
+
+    parameters = []
+    parameters.append({'name': 'bchprice', 'maxlength': 100})
+    bchprices = DataCollector(filename='bchprices', parameters=parameters, overwrite=False,
+                                   checklength=True)
+
     while True:
 
         try:
             # collect price every 5 minutes
             time.sleep(300)
 
-
             price, price_datetime = pull_data('USDT-BTC')
-
+            print('price: ', price)
             print('appending bitcoin price at {}'.format(datetime.now()))
             bittrexbtcprices.append_data(parameter='btcprice', data=[price], times=[price_datetime])
+            time.sleep(1)
+
+            price, price_datetime = pull_data('USD-USDT')
+            print('price: ', price)
+            print('appending tether price at {}'.format(datetime.now()))
+            bittrextetherprices.append_data(parameter='tetherprice', data=[price], times=[price_datetime])
+            time.sleep(1)
+
+            price, price_datetime = pull_data('USD-ETH')
+            print('price: ', price)
+            print('appending tether price at {}'.format(datetime.now()))
+            ethereumprices.append_data(parameter='ethereumprice', data=[price], times=[price_datetime])
+            time.sleep(1)
+
+            price, price_datetime = pull_data('USD-BCH')
+            print('price: ', price)
+            print('appending tether price at {}'.format(datetime.now()))
+            bchprices.append_data(parameter='bchprice', data=[price], times=[price_datetime])
+            time.sleep(1)
 
 
         except Exception as e:
